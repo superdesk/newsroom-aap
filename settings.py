1eb6d40784f1137840a6884851d28a713bff320f
Mayur Dhamanwala <mdhamanwala@aap.com.au>
2018-04-11 14:58:30 +1000

    Configuring newrelic for the application.

--- settings.py
+++ settings.py
@@ -1,8 +1,10 @@
 
 import os
-from newsroom.default_settings import BLUEPRINTS as blueprints
+from newsroom.default_settings import BLUEPRINTS as blueprints, CORE_APPS
 
 if os.environ.get('PUSH'):
     BLUEPRINTS = blueprints
 else:
     BLUEPRINTS = [blueprint for blueprint in blueprints if 'push' not in blueprint]
+
+CORE_APPS.append('instrumentation')
