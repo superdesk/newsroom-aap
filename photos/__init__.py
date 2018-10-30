85f1d41459ba441c86859ee95272d34ad666384c
Tolga Akin <akint@aap.com.au>
2018-10-30 23:55:28 +1100

    Created a photos module to update coverage delivery href

---
+++ photos/__init__.py
@@ -0,0 +1,14 @@
+from flask import current_app as app
+
+
+def set_photo_coverage_href(coverage, planning_item):
+    if app.config.get('PHOTO_URL') and \
+            coverage['planning']['g2_content_type'] == 'picture' and \
+            coverage['workflow_status'] == 'completed':
+        slugline = coverage.get('planning', {}).get('slugline', planning_item.get('slugline'))
+        q = '{"DateRange":[{"Start":"%s"}],"DateCreatedFilter":"true"}' % coverage['scheduled'][:10]
+        return '{}"{}"?q={}'.format(app.config.get('PHOTO_URL'), slugline, q)
+
+
+def init_app(app):
+    app.set_photo_coverage_href = set_photo_coverage_href
