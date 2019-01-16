680e6359401f0302887db63db4044055db04468d
nrvikas@gmail.com <nrvikas@gmail.com>
2019-01-16 18:15:44 +1100

    [SDAN-411] Style changes to banners and sidenavs

---
+++ photos/external_products.py
@@ -0,0 +1,9 @@
+def register_products(app):
+    app.sidenav('Photos', icon='coverage-photo',
+                url=app.config.get('MULTIMEDIA_WEBSITE_URL'), group=2)
+    app.sidenav('Videos', icon='coverage-video', group=2,
+                url=app.config.get('VIDEOS_WEBSITE_URL'))
+    app.sidenav('Explainers', icon='explainer',
+                url=app.config.get('EXPLAINERS_WEBSITE_URL'), group=2)
+    app.sidenav('Fact Check', icon='fact-check',
+                url=app.config.get('FACTCHECK_WEBSITE_URL'), group=2)
--- settings.py
+++ settings.py
@@ -28,4 +28,9 @@ DEFAULT_LANGUAGE = 'en'
 MULTIMEDIA_WEBSITE_URL = os.environ.get('MULTIMEDIA_WEBSITE_URL', 'https://photos.aap.com.au')
 MULTIMEDIA_WEBSITE_SEARCH_URL = '{}/{}/'.format(MULTIMEDIA_WEBSITE_URL,
                                                 os.environ.get('MULTIMEDIA_WEBSITE_SEARCH_PATH', 'search'))
+VIDEOS_WEBSITE_URL = os.environ.get('VIDEOS_WEBSITE_URL', 'https://photos.aap.com.au/galleries/Newsroom/Video')
+EXPLAINERS_WEBSITE_URL = os.environ.get('EXPLAINERS_WEBSITE_URL',
+                                        'https://photos.aap.com.au/search/(supplementalcategory'
+                                        ':VIDEXP)/Visual%20Explainers')
+FACTCHECK_WEBSITE_URL = os.environ.get('FACTCHECK_WEBSITE_URL', 'https://crosscheck.aap.com.au/')
 CLIENT_CONFIG['list_animations'] = False
