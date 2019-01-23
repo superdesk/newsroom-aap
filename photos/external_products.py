15d8a029040ef87aa989752e58dcd43a29daf6fd
nrvikas@gmail.com <nrvikas@gmail.com>
2019-01-23 15:36:34 +1100

    [SDAN-417][SDAN-418] Rename 'Saved Items' nav to 'Saved/Watched Items' and rename 'Fact Check' nav to 'CrossCheck'

--- photos/external_products.py
+++ photos/external_products.py
@@ -5,5 +5,5 @@ def register_products(app):
                 url=app.config.get('VIDEOS_WEBSITE_URL'))
     app.sidenav('Explainers', icon='explainer',
                 url=app.config.get('EXPLAINERS_WEBSITE_URL'), group=2)
-    app.sidenav('Fact Check', icon='fact-check',
+    app.sidenav('CrossCheck', icon='fact-check',
                 url=app.config.get('FACTCHECK_WEBSITE_URL'), group=2)
