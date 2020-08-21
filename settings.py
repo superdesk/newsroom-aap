4150ca73303086d91ae5f3206b88c200485b973f
marwoodandrew <amarwood@aap.com.au>
2020-08-21 12:33:31 +1000

    Release 1.19.1

--- settings.py
+++ settings.py
@@ -48,5 +48,6 @@ FACTCHECK_WEBSITE_URL = os.environ.get('FACTCHECK_WEBSITE_URL', 'https://www.aap
 CLIENT_CONFIG['list_animations'] = False
 CLIENT_CONFIG['multimedia_website_search_url'] = MULTIMEDIA_WEBSITE_SEARCH_URL
 BACK_STORY_URL = os.environ.get('BACK_STORY_URL', 'https://backstory.aap.com.au/')
+ALLOW_PICTURE_DOWNLOAD = False
 
 MONITORING_REPORT_NAME = 'Newswire'
