6cc78f79437baff486204eb7950103e78f488a08
marwoodandrew <amarwood@aap.com.au>
2021-08-23 10:54:43 +1000

    SDAN-704 Remove Back Story link

--- photos/external_products.py
+++ photos/external_products.py
@@ -3,5 +3,3 @@ def register_products(app):
                 url=app.config.get('MULTIMEDIA_WEBSITE_URL'), group=2)
     app.sidenav('FactCheck', icon='fact-check',
                 url=app.config.get('FACTCHECK_WEBSITE_URL'), group=2)
-    app.sidenav('Back Story', icon='books',
-                url=app.config.get('BACK_STORY_URL'), group=2)
