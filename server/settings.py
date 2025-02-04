f4b71f2f25990fb8f1d3da98a9e8dea78c77609e
Mark Pittaway <mark.pittaway@gmail.com>
2025-02-04 16:38:39 +1100

    Update settings

--- server/settings.py
+++ server/settings.py
@@ -1,12 +1,24 @@
 import os
 import pathlib
 
+from quart_babel import lazy_gettext
+
+from newsroom.web.default_settings import CLIENT_CONFIG
+
 SERVER_PATH = pathlib.Path(__file__).resolve().parent
 CLIENT_PATH = SERVER_PATH.parent.joinpath("client")
 
 WEBPACK_MANIFEST_PATH = os.environ.get(
     "WEBPACK_MANIFEST_PATH", CLIENT_PATH.joinpath("dist", "manifest.json")
 )
+
+CONTACT_ADDRESS = "https://www.aap.com.au/contact"
+SITE_NAME = "AAP Newsroom"
+COPYRIGHT_HOLDER = "AAP"
+COPYRIGHT_NOTICE = ""
+USAGE_TERMS = ""
+LANGUAGES = ["en"]
+DEFAULT_LANGUAGE = "en"
 
 WIRE_AGGS = {
     "genre": {"terms": {"field": "genre.name", "size": 50}},
@@ -16,5 +28,8 @@ WIRE_AGGS = {
     "place": {"terms": {"field": "place.name", "size": 50}},
 }
 
-SHOW_USER_REGISTER = True
-SIGNUP_EMAIL_RECIPIENTS = "admin@example.com"
+WIRE_SECTION = lazy_gettext("The Wire")
+
+CLIENT_CONFIG.update({
+    "list_animations": False,
+})
